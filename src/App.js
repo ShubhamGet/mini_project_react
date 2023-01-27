ca70fbf7480810f8db854fbcf8fe10ce5732fcfb
Shubham Kumar Singh <102406929+ShubhamGet@users.noreply.github.com>
2023-01-27 15:42:14 +0530

    second

--- src/App.js
+++ src/App.js
@@ -1,15 +1,10 @@
 import React from 'react'
 import Comp1 from './Components/Comp1';
-import Comp2 from './Components/Comp2';
-import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
-import Comp3 from './Components/Comp3';
 
 const App = () => {
   return (
     <>
       <Comp1 />
-      <Comp2 />
-      <Comp3 />
     </>
   )
 }
